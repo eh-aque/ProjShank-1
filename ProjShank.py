e43687f93a6ad4691c650a0b4df307e2cff81467
eh-aque <146104968+eh-aque@users.noreply.github.com>
2023-10-12 10:18:50 -0400

    swag mode v2
    commented out old code made new code more efficient

--- ProjShank.py
+++ ProjShank.py
@@ -1,26 +1,99 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 28 10:36:34 2023
+# #!/usr/bin/env python3
+# # -*- coding: utf-8 -*-
+# """
+# Created on Thu Sep 28 10:36:34 2023
 
-@author: ehaque
-may God help any soul that tries to interpret this code 🙏
+# @author: ehaque
+# may God help any soul that tries to interpret this code 🙏
 
-"""
+# """
+
+# import numpy as np
+# import sklearn as sk
+# import seaborn as sns
+# import pandas as pd
+# import matplotlib.pyplot as plt
+# import joblib
+# from sklearn.model_selection import StratifiedShuffleSplit
+# from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+# #2.1 Data Processing
+# df = pd.read_csv('Project 1 data.csv')
+
+# # test data reading -- print(df.head())
+
+# # Setting up chats I think?
+# #2.2 Data Visualization
+# from mpl_toolkits import mplot3d
+
+# fig = plt.figure()
+# ax = plt.axes(projection='3d')
+
+# ax.scatter3D(df['X'], df['Y'], df['Z']);
+# ax.view_init(0, 0) #plot orient x-y plane about z axis
+
+# # correlation heatmap stuff using Seaborn - with the aid of Seaborn
+# #2.3 Correlation Analysis
+# correlation_matrix = df.corr()
+
+# plt.figure(figsize=(10, 8))
+# sns.heatmap(correlation_matrix, annot=True, cmap='PiYG', fmt=".2f", linewidths=0.5)
+# plt.title("Pearson Correlation Matrix")
+# plt.show()
+
+# #2.4 Classification Model Dev
+# # get coordinates output step, therefore model should have same format
+# # split data frame into coord & step (2 frames liek 2 chainz)
+# # Split the DataFrame by columns
+# dfcoords = df[['X', 'Y', 'Z']]  # Create a DataFrame with columns X Y Z
+# dfstep = df[['Step']]  # Create a DataFrame with column Step
+
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.model_selection import train_test_split
+# # xyz input steps is output therefore train xyz to predict step
+
+# # Split the data into training and testing sets
+# # X_train, X_test, y_train, y_test, z_train, z_test = train_test_split(dfcoords[['X']], dfcoords[['Y']], dfcoords[['Z']], test_size=0.2, random_state=42)
+# coords_train, coords_test = train_test_split(dfcoords, test_size=0.2, random_state=42)
+
+# #feature scaling
+# scaler = StandardScaler()
+# coords_train = scaler.fit_transform(coords_train)
+# coords_test = scaler.transform(coords_test)
+
+# #classifier 
+# classifier = LogisticRegression()
+
+# classifier.fit(coords_train, coords_train)
+
+# step_pred = classifier.predict(coords_test)
+
+# from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+
+
+# accuracy = accuracy_score(coords_test, step_pred)
+# confusion = confusion_matrix(coords_test, step_pred)
+# report = classification_report(coords_test, step_pred)
+
+# print("Accuracy:", accuracy)
+# print("Confusion Matrix:\n", confusion)
+# print("Classification Report:\n", report)
+
 
 import numpy as np
-import sklearn as sk
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
 
-#2.1 Data Processing
+# Load the dataset
 df = pd.read_csv('Project 1 data.csv')
 
-# test data reading -- print(df.head())
 
-# Setting up chats I think?
+#2.2 Data Visualization
 from mpl_toolkits import mplot3d
 
 fig = plt.figure()
@@ -29,56 +102,72 @@ ax = plt.axes(projection='3d')
 ax.scatter3D(df['X'], df['Y'], df['Z']);
 ax.view_init(0, 0) #plot orient x-y plane about z axis
 
-# correlation heatmap stuff using Seaborn - with the aid of Seaborn
 
+# correlation heatmap stuff using Seaborn - with the aid of Seaborn
+#2.3 Correlation Analysis
 correlation_matrix = df.corr()
 
 plt.figure(figsize=(10, 8))
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f", linewidths=0.5)
+sns.heatmap(correlation_matrix, annot=True, cmap='PiYG', fmt=".2f", linewidths=0.5)
 plt.title("Pearson Correlation Matrix")
 plt.show()
 
-# #redoing dataframe for x y z axis
-# dfx = pd.read_csv('Project 1 data.csv', usecols=[0, 0])
-# dfy = pd.read_csv('Project 1 data.csv', usecols=[1, 1])
-# dfz = pd.read_csv('Project 1 data.csv', usecols=[2, 2])
 
-# # Data for three-dimensional scattered points
-# zdata = dfz
-# xdata = dfx
-# ydata = dfy
-# ax.scatter3D(xdata.values, ydata.values, zdata.values);
-# '''
-# ax.contour3D(xdata~, ydata, zdata, 50, cmap='binary')
-# ax.set_xlabel('x')
-# ax.set_ylabel('y')
-# ax.set_zlabel('z');'''
+#2.4 Classification Model Dev
+# Separate features (X: 'X', 'Y', 'Z') and target (y: 'Step')
+coords = df[['X', 'Y', 'Z']]
+step = df['Step']
 
-# # sorting by steps 1-13
+# Split the data into training and testing sets
+coords_train, coords_test, step_train, step_test = train_test_split(coords, step, test_size=0.2, random_state=42)
 
-# # a lil autistic this code is 
-# #dfSTEP = pd.read_csv('Project 1 data.csv', usecols = [3,3])
+# Feature scaling
+scaler = StandardScaler()
+coords_train = scaler.fit_transform(coords_train)
+coords_test = scaler.transform(coords_test)
 
-# # if dfSTEP == 1:
-#   #  dfS1 = pd.read_csv('Project 1 data.csv', dfSTEP.iloc[0,25])
-#     # idk why I went yoda mode fr
-    
-# # create loop 
-    
-#     # Read the CSV file with specific columns
-# dfSTEP = pd.read_csv('Project 1 data.csv', usecols=[3])
+# Create a Linear Regression model
+regressor = LinearRegression()
 
-# # Define a condition (e.g., rows where column 1 equals 1)
-# #condition = dfSTEP['Step'] == 1
+# Train the model
+regressor.fit(coords_train, step_train)
 
-# # Create a new DataFrame containing rows that meet the condition
-# #dfS1 = dfSTEP[condition]
+# Make predictions
+step_pred = regressor.predict(coords_test)
 
-# # Create a list to store the DataFrames
-# dfS_list = []
+# Evaluate the model's performance
+mse = mean_squared_error(step_test, step_pred)
+r2 = r2_score(step_test, step_pred)
 
-# # Create DataFrames for steps 1 to 13
-# for step in range(1, 14):
-#     condition = dfSTEP['Step'] == step
-#     dfS = dfSTEP[condition]
-#     dfS_list.append(dfS)
+print("Mean Squared Error:", mse)
+print("R-squared:", r2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
