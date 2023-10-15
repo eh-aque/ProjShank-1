94d96d068c0694868771419e4ec8a607cd65d02d
eh-aque <146104968+eh-aque@users.noreply.github.com>
2023-10-14 21:16:58 -0400

    3 model working
    3 model with working outputs!

--- ProjShank.py
+++ ProjShank.py
@@ -2,30 +2,30 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.svm import SVC
-#from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.metrics import mean_squared_error, r2_score
+
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 # Load the dataset
 df = pd.read_csv('Project 1 data.csv')
 
-
-#2.2 Data Visualization
-from mpl_toolkits import mplot3d
+# Data Visualization
+from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure()
-ax = plt.axes(projection='3d')
+ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter3D(df['X'], df['Y'], df['Z']);
-ax.view_init(0, 0) #plot orient x-y plane about z axis
+ax.scatter(df['X'], df['Y'], df['Z'])
+ax.view_init(30, 185)  # Set the view angle
 
+plt.show()
 
-# correlation heatmap stuff using Seaborn - with the aid of Seaborn
-#2.3 Correlation Analysis
+# Correlation Analysis
 correlation_matrix = df.corr()
 
 plt.figure(figsize=(10, 8))
@@ -33,8 +33,7 @@ sns.heatmap(correlation_matrix, annot=True, cmap='PiYG', fmt=".2f", linewidths=0
 plt.title("Pearson Correlation Matrix")
 plt.show()
 
-
-#2.4 Classification Model Dev
+# Classification Model Development
 # Separate features (X: 'X', 'Y', 'Z') and target (y: 'Step')
 coords = df[['X', 'Y', 'Z']]
 step = df['Step']
@@ -47,105 +46,33 @@ scaler = StandardScaler()
 coords_train = scaler.fit_transform(coords_train)
 coords_test = scaler.transform(coords_test)
 
-
-
-'''model 1 Linear Regression'''
-
-# Create a Linear Regression model
-"""11111111111111111111"""
+# Model 1: Linear Regression
 linear_regressor = LinearRegression()
-
-# Train the model
 linear_regressor.fit(coords_train, step_train)
-
-# Make predictions
 linear_predictions = linear_regressor.predict(coords_test)
-
-# Evaluate the model's performance
-
-#mse = mean_squared_error(step_test, step_pred)
-#r2 = r2_score(step_test, step_pred)
-#print("Mean Squared Error:", mse)
-#print("R-squared:", r2)
+mse_linear = mean_squared_error(step_test, linear_predictions)
+r2_linear = r2_score(step_test, linear_predictions)
 
 print("Linear Regression:")
-print("Accuracy:", accuracy_score(step_test,linear_predictions))
-print("Confusion Matrix:\n", confusion_matrix(step_test, linear_predictions))
-print("Classification Report:\n", classification_report(step_test,linear_predictions))
+print("Mean Squared Error:", mse_linear)
+print("R-squared:", r2_linear)
 
-
-
-'''model 2 SVM'''
-
-# Create a Support Vector Machine model
-"""22222222222222222222"""
+# Model 2: Support Vector Machine (SVM)
 svm_classifier = SVC()
-
-# Train the model
 svm_classifier.fit(coords_train, step_train)
-
-# Make predictions
 svm_predictions = svm_classifier.predict(coords_test)
 
-# Evaluate the model's performance
-
-#mse = mean_squared_error(step_test, step_pred)
-#r2 = r2_score(step_test, step_pred)
-#print("Mean Squared Error:", mse)
-#print("R-squared:", r2)
-
 print("Support Vector Machine:")
-print("Accuracy:", accuracy_score(step_test,svm_predictions))
-print("Confusion Matrix:\n", confusion_matrix(step_test, svm_predictions))
-print("Classification Report:\n", classification_report(step_test,svm_predictions))
+print("Accuracy:", accuracy_score(step_test,svm_predictions))  # This should be replaced with appropriate regression metric
+print("Confusion Matrix:\n", confusion_matrix(step_test, svm_predictions))  # This should be removed for regression
+print("Classification Report:\n", classification_report(step_test,svm_predictions))  # This should be removed for regression
 
-
-
-'''model 3 Random Forrest'''
-
-# Create a Random Forrest model
-"""*3333333333333333333"""
+# Model 3: Random Forest Classifier
 rf_classifier = RandomForestClassifier()
-
-# Train the model
 rf_classifier.fit(coords_train, step_train)
-
-# Make predictions
 rf_predictions = rf_classifier.predict(coords_test)
 
-# Evaluate the model's performance
-
-#mse = mean_squared_error(step_test, step_pred)
-#r2 = r2_score(step_test, step_pred)
-#print("Mean Squared Error:", mse)
-#print("R-squared:", r2)
-
 print("Random Forest Classifier:")
-print("Accuracy:", accuracy_score(step_test,rf_predictions))
+print("Accuracy:", accuracy_score(step_test, rf_predictions))
 print("Confusion Matrix:\n", confusion_matrix(step_test, rf_predictions))
-print("Classification Report:\n", classification_report(step_test,rf_predictions))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print("Classification Report:\n", classification_report(step_test, rf_predictions))  # This should be removed for regression
